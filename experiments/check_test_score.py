74537364329ca47e97bbb3a17ac867378a2cfad2
upura <upura0@gmail.com>
2020-07-12 23:45:35 +0900

    fe003

--- experiments/check_test_score.py
+++ experiments/check_test_score.py
@@ -4,7 +4,7 @@ from ayniy.utils import Data
 
 
 if __name__ == '__main__':
-    pred = Data.load('../output/pred/run000-test.pkl')
-    y_test = Data.load('../input/y_test_fe000.pkl')
+    pred = Data.load('../output/pred/run003-test.pkl')
+    y_test = Data.load('../input/y_test_fe003.pkl')
     print(roc_auc_score(y_test, pred))
     print(confusion_matrix(y_test, (pred > 0.5).astype(int)))
